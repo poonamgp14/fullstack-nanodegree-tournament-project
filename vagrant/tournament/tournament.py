70bc8440df3678988508962585028790b43f7d47
Poonam Gupta <ppgupt1@gmail.com>
2017-01-19 22:40:24 -0600

    table doesnot exits error

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,25 +13,45 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM "tournament"."Matches"')
+    # cursor.execute('DELETE FROM "Matches"')
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM "tournament"."Players"')
+    conn.commit()
+    conn.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    cursor = conn.cursor()
+    result = cursor.execute('SELECT COUNT(*) FROM "tournament"."Players"')
+    return result.fetchone()
+
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO tournament.players(Name) VALUES ('%s')" % name)
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -56,16 +76,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,24 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE "tournament";
+DROP TABLE "Players";
+DROP TABLE "Matches";
+CREATE DATABASE "tournament";
 
+--CREATE TABLE (with auto numbering integer id)
+
+CREATE TABLE "Players" (
+	Id serial PRIMARY KEY,
+	Name varchar(50) NOT NULL,
+	CreatedDate timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE "Matches" (
+	Id serial PRIMARY KEY,
+	Player1 int NOT NULL,
+	Player2 int NOT NULL,
+	Winner int NOT NULL,
+	Loser int NOT NULL
+
+);
